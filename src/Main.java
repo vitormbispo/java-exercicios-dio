c58cf08764a2047f4730dd397fbde9c693c3fd60
VitorBispo <vitormartbispo@gmail.com>
2025-08-08 21:20:37 -0300

    Implementada escolha de temas de exercícos no programa principal.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import Exercicios.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ExerciciosEstruturasDeControle.Exercicio2();
+        int opcao = 0;
+        boolean programaEncerrado = false;
+        do {
+            System.out.println("=== Lista de Exercícios Java DIO ===");
+
+            System.out.println("\n\n1) Fundamentos de Java.");
+            System.out.println("2) Estruturas de Controle.");
+            System.out.println("3) Encerrar aplicação");
+            System.out.print("\n Escolha um dos temas:   ");
+
+            Scanner scan = new Scanner(System.in);
+            opcao = scan.nextInt();
+            scan.nextLine();
+
+            switch (opcao) {
+                case 1 -> ExerciciosFundamentos.main(null);
+                case 2 -> ExerciciosEstruturasDeControle.main(null);
+                default -> programaEncerrado = true;
+            }
+        } while (!programaEncerrado);
+        System.out.println("Encerrando aplicação...");
     }
+
 }
